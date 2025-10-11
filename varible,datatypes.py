dfa457265ee84dc9433451e45a1a5cae03d018f5
Anurag Nayak <anu870906@gmail.com>
2025-10-11 21:08:12 +0530

    Add files via upload

---
+++ varible,datatypes.py
@@ -0,0 +1,41 @@
+# Arithmetic Operators
+a = 5
+b = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) # float values
+print(a//b) # int values
+print(a%b) # reminder
+print(a**b) # power
+
+# Relational Operator
+
+print(a==b)
+# == != > < >= <=  this return always boolean value
+
+# Assigment operator ( = )
+
+a = b
+print(a)
+# += *= -= /= **= are the Assigment operators
+
+# Type Conversion
+
+i = int("23")
+print(type(i))
+print(i+2)
+
+#   input
+
+# by default the input funtion taked input as a string we need to type cast it
+name = str(input())
+age = int(input())
+print(name)
+print(age)
+
+first = float(input())
+second = float(input())
+
+print((first + second)/2)
